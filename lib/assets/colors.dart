763f6188e01aa2b06e534849851daadc4b3ae91e
dbspoudel <dbspoudel@gmail.com>
2020-11-04 11:49:29 +0545

    textStyle & color pattern file added

---
+++ lib/assets/colors.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class SmartMobeTaskColors {
+  /// Grey and shades
+  static const Color grey = Color(0xFF546E7A);
+  static const Color grey80 = Color(0xCC546E7A);
+  static const Color grey60 = Color(0xA6546E7A);
+  static const Color grey40 = Color(0x66546E7A);
+  static const Color grey20 = Color(0x40546E7A);
+  static const Color grey10 = Color(0x1A546E7A);
+
+  /// Amber and shades
+  static const Color amber = Color(0xFFEE9E0C);
+  static const Color amber80 = Color(0xCCEE9E0C);
+  static const Color amber60 = Color(0xA6EE9E0C);
+  static const Color amber40 = Color(0x66EE9E0C);
+  static const Color amber20 = Color(0x40EE9E0C);
+  static const Color amber10 = Color(0x1AEE9E0C);
+
+  /// Black and shades
+  static const Color black = Color(0xFF313131);
+  static const Color blackGrey = Color(0xFF6D6D6D);
+
+  /// White
+  static const Color white = Color(0xFFFFFFFF);
+
+  /// Drawer colors
+  static const Color drawerBackground = Color(0xFF2B2A30);
+  static const Color drawerMenuIcon = Color(0xFFFFFFFF);
+  static const Color drawerMenuText = Color(0xFFFFFFFF);
+}
---
+++ lib/assets/text_style.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:smart_mobe_task/assets/colors.dart';
+
+class SmartMobeTextStyle {
+  static final TextStyle h3 = TextStyle(
+    color: SmartMobeTaskColors.black,
+    fontSize: 42,
+    fontWeight: FontWeight.w600,
+    letterSpacing: 0,
+  );
+  static final TextStyle h4 = TextStyle(
+    color: SmartMobeTaskColors.black,
+    fontSize: 28,
+    fontWeight: FontWeight.w600,
+    letterSpacing: 0,
+  );
+  static final TextStyle h5 = TextStyle(
+    color: SmartMobeTaskColors.black,
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+    letterSpacing: 0,
+  );
+  static final TextStyle h6 = TextStyle(
+    color: SmartMobeTaskColors.black,
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    letterSpacing: 0.25,
+  );
+
+  static final TextStyle menuItemTextStyle = TextStyle(
+    color: SmartMobeTaskColors.drawerMenuText,
+    fontSize: 20,
+  );
+  static final TextStyle permanentMarker14Grey = TextStyle(
+    color: SmartMobeTaskColors.black,
+    fontSize: 14,
+  );
+}
