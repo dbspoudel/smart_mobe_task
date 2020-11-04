e76800cccee9eb1aee20a0d7d7f7f30c106a5859
dbspoudel <dbspoudel@gmail.com>
2020-11-04 11:51:58 +0545

    pet selector widget created

---
+++ lib/ui/widgets/pet_selector.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:smart_mobe_task/assets/colors.dart';
+import 'package:smart_mobe_task/assets/text_style.dart';
+
+class PetSecectorDataModel {
+  final String label;
+  final Widget icon;
+
+  PetSecectorDataModel({
+    @required this.label,
+    this.icon,
+  });
+}
+
+class PetSelector extends StatefulWidget {
+  final List<PetSecectorDataModel> optionList;
+  final Function(String) onChangeEventHandler;
+
+  const PetSelector({
+    @required this.optionList,
+    @required this.onChangeEventHandler,
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _PetSelectorState createState() => _PetSelectorState();
+}
+
+class _PetSelectorState extends State<PetSelector> {
+  String selectedItem;
+
+  onchanged(String newValue) {
+    setState(() {
+      selectedItem = newValue;
+    });
+    widget.onChangeEventHandler(newValue);
+  }
+
+  Widget _buildSingleItem(String title, Widget icon) {
+    bool isSelected = title == selectedItem;
+    return GestureDetector(
+      onTap: () {
+        if (!isSelected) onchanged(title);
+      },
+      child: Container(
+        height: 60.0,
+        decoration: BoxDecoration(
+            color: isSelected ? SmartMobeTaskColors.grey10 : Colors.transparent,
+            borderRadius: BorderRadius.circular(30.0),
+            border: Border.all(
+              color: SmartMobeTaskColors.grey10,
+              width: 2.0,
+            )),
+        padding: EdgeInsets.only(
+          left: 22.0,
+          right: 10.0,
+        ),
+        margin: EdgeInsets.symmetric(
+          horizontal: 12.0,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "$title",
+              style: SmartMobeTextStyle.h6.copyWith(fontSize: 18.0),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 4.0),
+              child: icon ?? SizedBox.shrink(),
+            ),
+            Radio(
+              value: "$title",
+              groupValue: selectedItem,
+              onChanged: (_) => null,
+              activeColor: SmartMobeTaskColors.black,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: widget.optionList
+          .map((e) => _buildSingleItem(e.label, e.icon))
+          .toList(),
+    );
+  }
+}
